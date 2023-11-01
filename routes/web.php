1e5d95edf04bd2eccf5911b295297e2477d872af
samuelebruni <samuele_bruni@hotmail.com>
2023-11-01 15:38:57 +0100

    create admin and user controllers and connect to app\models\comic

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\User\PageController;
 
 Route::get('/', function () {
     return view('welcome');
